1ed53a88381620b420662a3ebc5a0de6676f1bef
KazThomas <mr.firebass@gmail.com>
2024-10-05 20:13:32 +0100

    Fixed animation bug + Spawns only once
    spawning only spawns one note flat out, doesnt spawn anymore afterwards, next update will be the fix for it

--- Scripts/Beat1Test.gd
+++ Scripts/Beat1Test.gd
@@ -1,15 +1,13 @@
 extends Node2D
 
-
-
 @export var highP := 0.3
 @export var lowP := -0.3
 var pitch: float = randf_range( lowP, highP)
 
+#var beat = load("res://Scenes/Beat1Test.tscn").instantiate()
+
 func _ready():
 	$AudioStreamPlayer.pitch_scale += pitch
 	$"1Beat".modulate = Color.DARK_CYAN
-
-	print(pitch)
 
 
--- Scripts/mainScene.gd
+++ Scripts/mainScene.gd
@@ -6,6 +6,7 @@ extends Node
 @onready var point_2 = $spawnZone/point2
 @onready var vinyl_record = $VinylRecord
 @onready var space_bar_ui = $Menu/SpaceBarUi
+var beat = load("res://Scenes/Beat1Test.tscn").instantiate()
 @onready var bankArray = [$Banks/Bank, $Banks/Bank2, $Banks/Bank3, $Banks/Bank4]
 
 @onready var mouse_x = get_viewport().get_mouse_position().x
@@ -15,14 +16,18 @@ var canDrop : bool = false
 
 var vinylAnim : bool = true
 
+signal spawn_note
+
 func _ready():
 	print(bankArray[1]) #testing array works
 
 func _input(event):
+	
 	mouse_x = get_viewport().get_mouse_position().x
-	var tween = create_tween()
+	
 	if event.is_action_pressed("add_layer") && vinylAnim == true: # 'activates' the game
 		space_bar_ui.visible = false
+		var tween = create_tween()
 		tween.tween_property(vinyl_record, "position", Vector2(1500, 500), 1.5)
 		tween.tween_property(vinyl_record, "scale", Vector2(0.45, 0.45), 0.3)
 		tween.tween_property(vinyl_record, "position", Vector2(576, 489), 1)
@@ -31,8 +36,11 @@ func _input(event):
 		layer.visible = true
 		for bank in bankArray:
 			bank.visible = true
-	if layer.visible == true:
-		canDrop = true
+	if layer.visible == true && event.is_action_pressed("drop"):
+		if canDrop == false:
+			spawn_note.emit()
+			canDrop = true
+		
 		#if event.is_action_pressed("bankIt"): #only adds it to the first bank, need to find iteration
 		#	remove_child(beat)
 		#	beat.position = bankArray[0].position
@@ -43,9 +51,9 @@ func _input(event):
 		#	beat.position.x = mouse_x
 		#	add_child(beat)
 
-func notesFalling():
+func _on_spawn_note():
 	var rest = load("res://Scenes/rest_beat.tscn").instantiate()
-	var beat = load("res://Scenes/Beat1Test.tscn").instantiate() #if its declared at top - dont work
+	 #if its declared at top - dont work
 	var randomNote : int =  randi_range(1, 2)
 	
 	var y_pos = roof.position.y + 20
@@ -61,10 +69,11 @@ func notesFalling():
 				rest.position = Vector2(mouse_x, y_pos)
 				add_child(rest)
 
-func _process(_delta):
-	if canDrop == true:
-		notesFalling()	
-	
+#NEED TO SPAWN ONE AFTER ANOTHER
+#func _process(delta):
+#	if beat.velocity. y <= 0:
+#		canDrop = false
+
 # NEED TO CHECK IF OBJECT IS FALLING
 # NEED TO SEE IF BANK IS HIGHLIGHTED
 # NEED TO CHECK IF BANK IS OCCUPIED
